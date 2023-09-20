20ffa5ae41f9534d0c3c2d52826a492c28364521
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2023-09-20 14:44:13 +0700

    added yup and formik validation

--- IntroductionToReact/fakestore-app/src/components/Input.jsx
+++ IntroductionToReact/fakestore-app/src/components/Input.jsx
@@ -1,32 +1,52 @@
-import {EyeIcon, EyeSlashIcon} from "@heroicons/react/24/solid"
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
 
-const Input = ({id, label, value, type, placeholder, onChange, onVisible, name, visibility}) => {
+const Input = ({
+  id,
+  label,
+  value,
+  type,
+  placeholder,
+  onChange,
+  onVisible,
+  name,
+  visibility,
+  register,
+  error,
+  ...props
+}) => {
   return (
-   <div className={`w-full h-full grid grid-cols-1 gap-y-1`}>
-        <label>{label}</label>
-        <input
+    <div className={`w-full h-full grid grid-cols-1 gap-y-1`}>
+      <label>{label}</label>
+      <input
         id={id}
         name={name}
         value={value}
         type={type}
         onChange={onChange}
         placeholder={placeholder}
-        className={`w-full h-full rounded-md shadow-md focus:outline-none p-3 bg-white`}
-        />
-        {
-            name === "password" &&
-            (
-                visibility === true ?
-                <div className="cursor-pointer" onClick={onVisible}>
-                   <EyeIcon width={30} height={30}/>
-                </div> :
-                 <div className="cursor-pointer" onClick={onVisible}>
-                    <EyeSlashIcon width={30} height={30}/>
-                </div>
-            )
-        }
-   </div>
-  )
-}
+        {...(register ? register(name) : {})}
+        {...props}
+        className={`w-full h-full rounded-md shadow-md focus:outline-none p-3 bg-white ${
+          error && "border-red-500"
+        }`}
+      />
+      {error && (
+        <label className="label">
+          <p className="text-red-500 text-md">{error}</p>
+        </label>
+      )}
+      {name === "password" &&
+        (visibility === true ? (
+          <div className="cursor-pointer" onClick={onVisible}>
+            <EyeIcon width={30} height={30} />
+          </div>
+        ) : (
+          <div className="cursor-pointer" onClick={onVisible}>
+            <EyeSlashIcon width={30} height={30} />
+          </div>
+        ))}
+    </div>
+  );
+};
 
-export default Input
+export default Input;
--- IntroductionToReact/fakestore-app/src/pages/login/index.jsx
+++ IntroductionToReact/fakestore-app/src/pages/login/index.jsx
@@ -1,57 +1,69 @@
-import React, {useState} from 'react'
-import {useNavigate} from "react-router-dom"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useFormik, validateYupSchema } from "formik";
+import * as yup from "yup";
 
-import Layout from '../../components/Layout'
-import Input from '../../components/Input'
-import Button from '../../components/Button'
+import Layout from "../../components/Layout";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [visibility, setVisibility] = useState(false);
 
-  const navigate = useNavigate()
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const [visibility, setVisibility] = useState(false)
+  const shape = {
+    username: yup.string().required("Username belum diisi"),
+    password: yup.string().required("Password belum diisi"),
+  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    navigate('/')
-  }
+  const userCredentials = yup.object().shape(shape);
 
-  console.log("visible ", visibility)
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: userCredentials,
+    onSubmit: (values) => {
+      console.log(values);
+      // navigate("/");
+    },
+  });
+
+  console.log("visible ", visibility);
 
   return (
     <Layout>
-      <form 
-      onSubmit={handleSubmit}
-      className='w-96 grid grid-cols-1 gap-y-5'>
-          <Input
+      <form
+        onSubmit={formik.handleSubmit}
+        className="w-96 grid grid-cols-1 gap-y-5"
+      >
+        <Input
           id="username"
           name="username"
           type="text"
           label="Username"
           placeholder="Type your username here ..."
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          />
-           <Input
+          value={formik.values.username}
+          onChange={formik.handleChange}
+          error={formik.errors.username}
+        />
+        <Input
           id="password"
           name="password"
           type={visibility === true ? "text" : "password"}
           label="Password"
           placeholder="Type your password here ..."
-          value={password}
+          value={formik.values.password}
           visibility={visibility}
           onVisible={() => setVisibility(!visibility)}
-          onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button
-          id="submit"
-          type="submit"
-          label="Submit"
-          />
+          onChange={formik.handleChange}
+          error={formik.errors.password}
+        />
+        <Button id="submit" type="submit" label="Submit" />
       </form>
     </Layout>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
